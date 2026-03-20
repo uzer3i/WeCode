df59435603b10624193da4a52ae6ac43a072d1fe
Aryan Jain <aryansinghai214@gmail.com>
2026-03-20 16:19:46 +0530

    some changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
-// connectDB();
+connectDB();
 
 app.listen("3000", () => {
   console.log("Server is listening on port 3000");
