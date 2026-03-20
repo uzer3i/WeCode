3f51e76dbaf3d7c88ca94ded4cdd3f3c3ed592bb
Aryan Jain <aryansinghai214@gmail.com>
2026-03-20 16:04:33 +0530

    fix DB setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config()
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
-// connectDB();
+connectDB();
 
 app.listen("3000", () => {
   console.log("Server is listening on port 3000");
